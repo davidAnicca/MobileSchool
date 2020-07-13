25bf0b60312ea37f69fb14bab4c38a6c8be50b79
David Meresan <meresan02david1006@gmail.com>
2020-07-13 16:24:13 +0300

    mouth selector done

--- app/src/main/java/com/kikoteam/mobileschool/avatar/MouthSelector.java
+++ app/src/main/java/com/kikoteam/mobileschool/avatar/MouthSelector.java
@@ -2,15 +2,50 @@ package com.kikoteam.mobileschool.avatar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.kikoteam.mobileschool.R;
 
 public class MouthSelector extends AppCompatActivity {
 
+    Avatar avatar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mouth_selector);
+
+        avatar = Avatar.getInstance();
+
+        ImageView finalForm = findViewById(R.id.mouthSelectorFinalForm);
+        finalForm.setImageBitmap(avatar.getFinalForm());
+
+        showOptions();
+
     }
+
+    private void showOptions(){
+        ImageView option1 = findViewById(R.id.mouthSelectorOption1);
+        option1.setImageBitmap(BitmapFactory.decodeResource(MouthSelector.this.getResources(), R.drawable.mouth_open));
+        ImageView option2 = findViewById(R.id.mouthSelectorOption2);
+        option2.setImageBitmap(BitmapFactory.decodeResource(MouthSelector.this.getResources(), R.drawable.mouth_almost_closed));
+    }
+
+    public void nextSelector(View view){
+        Intent intent = new Intent(this, HairSelector.class);
+        startActivity(intent);
+    }
+
+    public void selectOption(View view){
+
+        avatar.setMouth(ImageProcessor.getBitmapFromView(view));
+        avatar.rebuild();
+        ImageView finalForm = findViewById(R.id.mouthSelectorFinalForm);
+        finalForm.setImageBitmap(avatar.getFinalForm());
+    }
+
 }
