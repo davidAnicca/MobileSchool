00bb049c85ef423f1df708eb9bc2741f6013529c
Stefan-Dragan <stefandragan85@yahoo.com>
2020-07-29 11:59:17 +0300

    beautiful AgreeActivity

--- app/src/main/java/com/kikoteam/mobileschool/registerLogIn/AgreeActivity.java
+++ app/src/main/java/com/kikoteam/mobileschool/registerLogIn/AgreeActivity.java
@@ -1,9 +1,12 @@
 package com.kikoteam.mobileschool.registerLogIn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import pl.droidsonroids.gif.GifImageView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -14,26 +17,51 @@ import com.kikoteam.mobileschool.avatar.newAvatar.BaseSelector;
 public class AgreeActivity extends AppCompatActivity {
 
     Switch agreeSwitch;
+    GifImageView bubbles1;
+    GifImageView bubbles2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agree);
-        agreeSwitch = (Switch) findViewById(R.id.agreeSwitch);
+        setViews();
+        initializeViews();
 
         agreeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     Toast.makeText(getBaseContext(), getString(R.string.thank), Toast.LENGTH_SHORT).show();
-                    openNextActivity();
+
+                    bubbles1.setVisibility(View.VISIBLE);
+                    bubbles2.setVisibility(View.VISIBLE);
+
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        public void run() {
+                            openNextActivity();
+                        }
+                    }, 3000);   //5 seconds
+
                 }
             }
         });
 
     }
 
-    public void openNextActivity(){
+    private void setViews() {
+        bubbles1 = findViewById(R.id.agreeBubbles);
+        bubbles2 = findViewById(R.id.agreeBubbles2);
+        agreeSwitch = (Switch) findViewById(R.id.agreeSwitch);
+    }
+
+    private void initializeViews() {
+        bubbles1.setVisibility(View.INVISIBLE);
+        bubbles2.setVisibility(View.INVISIBLE);
+    }
+
+    private void openNextActivity() {
         Intent intent = new Intent(this, BaseSelector.class);
         startActivity(intent);
         finish();
